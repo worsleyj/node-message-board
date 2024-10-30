e7f19580cdfbd1b8fb07a69ca8cb04fa49e1657a
worsleyj <josephworsley23@gmail.com>
2024-10-30 16:14:43 -0400

    add /new

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -4,5 +4,8 @@ const indexRouter = Router();
 indexRouter.get("/", (req, res) => {
   res.render("index", { message: "Hello world" });
 });
+indexRouter.get("/new", (req, res) => {
+  res.send("new message");
+});
 
 module.exports = indexRouter;
