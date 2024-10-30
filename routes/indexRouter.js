02b3c17e899e37e2771a058c7c62459a00f0a5e4
worsleyj <josephworsley23@gmail.com>
2024-10-30 16:23:49 -0400

    add view for default messages

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,8 +1,21 @@
 const { Router } = require("express");
 const indexRouter = Router();
 
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
+
 indexRouter.get("/", (req, res) => {
-  res.render("index", { message: "Hello world" });
+  res.render("index", { messages: messages });
 });
 indexRouter.get("/new", (req, res) => {
   res.send("new message");
